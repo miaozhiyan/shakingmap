c46f65a579b4c691f9fa1993986c63021d0943fb
缪执言 <283206322@qq.com>
2018-03-23 22:34:41 +0800

    新建项目

---
+++ src/main/java/com/douyu/superman/shakingmap/ShakingmapApplication.java
@@ -0,0 +1,12 @@
+package com.douyu.superman.shakingmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakingmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakingmapApplication.class, args);
+	}
+}
---
+++ src/test/java/com/douyu/superman/shakingmap/ShakingmapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.douyu.superman.shakingmap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShakingmapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
