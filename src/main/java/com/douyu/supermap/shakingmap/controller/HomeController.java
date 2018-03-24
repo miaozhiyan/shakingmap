d7eee8c2f50fee6c22c6fa9dc3939b3254c29347
缪执言 <283206322@qq.com>
2018-03-24 23:30:50 +0800

    thymeleaf 问题解决

--- src/main/java/com/douyu/supermap/shakingmap/controller/HomeController.java
+++ src/main/java/com/douyu/supermap/shakingmap/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.douyu.supermap.shakingmap.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("name","mzy");
         return "index";
     }
 }
